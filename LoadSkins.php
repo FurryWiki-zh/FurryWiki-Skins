e3b88a70dd1ff534ddf642b1e47941db55014af5
海屿有燕 <zorua@vip.qq.com>
2026-02-12 23:02:37 +0800

    添加子模块和加载皮肤功能

---
+++ LoadSkins.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!function_exists('wfLoadSkin')) {
+    function wfLoadSkin($name, $path = null) {
+        /// 伪定义，仅用于静态分析工具
+    }
+}
+
+$furwikiSkinsPath = "/www/wwwroot/mw-utils/FurryWiki-Skins";
+$furwikiSettingsPath = "/www/wwwroot/mw-utils/FurryWiki-Settings";
+
+wfLoadSkin( 'Vector', "$furwikiSkinsPath/Vector/skin.json" );
+wfLoadSkin( 'Citizen', "$furwikiSkinsPath/Citizen/skin.json" );
+wfLoadSkin( 'Timeless', "$furwikiSkinsPath/Timeless/skin.json" );
